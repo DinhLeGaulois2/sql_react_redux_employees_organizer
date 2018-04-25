5c1b4465e19ff6df39649f8464ae907ff801ea01
dinhlegaulois2 <dinh.hu87@yahoo.com>
2018-04-24 21:29:59 -0400

    Reducer: Done

--- src/reducers/employees_organizer/employees_organizer_reducer.js
+++ src/reducers/employees_organizer/employees_organizer_reducer.js
@@ -8,10 +8,112 @@ const initialStates = {
 
 const myReducer = (state = initialStates, action) => {
     switch (action.type) {
-
-        case cst.MENU_ADD: {
+        case cst.ADD_DPT: {
             return Object.assign({}, state, {
-                menuStatus: action.type
+                status: action.type
+            })
+        }
+        case cst.ADD_DPT_SUCCESS: {
+            return Object.assign({}, state, {
+                status: action.type
+            })
+        }
+        case cst.ADD_EMP: {
+            return Object.assign({}, state, {
+                status: action.type
+            })
+        }
+        case cst.ADD_EMP_SUCCESS: {
+            return Object.assign({}, state, {
+                status: action.type
+            })
+        }
+        case cst.ADD_SALARY: {
+            return Object.assign({}, state, {
+                status: action.type
+            })
+        }
+        case cst.ADD_SALARY_SUCCESS: {
+            return Object.assign({}, state, {
+                status: action.type
+            })
+        }
+        case cst.ADD_TITLE: {
+            return Object.assign({}, state, {
+                status: action.type
+            })
+        }
+        case cst.ADD_TITLE_SUCCESS: {
+            return Object.assign({}, state, {
+                status: action.type
+            })
+        }
+        case cst.DISPLAY_DPT_BY_ID: {
+            return Object.assign({}, state, {
+                status: action.type,
+                data: action.payload
+            })
+        }
+        case cst.DISPLAY_DPT_BY_ID_SUCCESS: {
+            return Object.assign({}, state, {
+                status: action.type,
+                data: action.payload
+            })
+        }
+        case cst.DISPLAY_DPT: {
+            return Object.assign({}, state, {
+                status: action.type,
+                data: action.payload
+            })
+        }
+        case cst.DISPLAY_DPT_SUCCESS: {
+            return Object.assign({}, state, {
+                status: action.type,
+                data: action.payload
+            })
+        }
+        case cst.DISPLAY_EMP_BY_ID: {
+            return Object.assign({}, state, {
+                status: action.type,
+                data: action.payload
+            })
+        }
+        case cst.DISPLAY_EMP_BY_ID_SUCCESS: {
+            return Object.assign({}, state, {
+                status: action.type,
+                data: action.payload
+            })
+        }
+        case cst.DISPLAY_EMP: {
+            return Object.assign({}, state, {
+                status: action.type,
+                data: action.payload
+            })
+        }
+        case cst.DISPLAY_EMP_SUCCESS: {
+            return Object.assign({}, state, {
+                status: action.type,
+                data: action.payload
+            })
+        }
+        case cst.LINK_EMP_2_DPT: {
+            return Object.assign({}, state, {
+                status: action.type
+            })
+        }
+        case cst.LINK_EMP_2_DPT_SUCCESS: {
+            return Object.assign({}, state, {
+                status: action.type
+            })
+        }
+        case cst.LINK_MANAGER_2_DPT: {
+            return Object.assign({}, state, {
+                status: action.type
+            })
+        }
+        case cst.LINK_MANAGER_2_DPT_SUCCESS: {
+            return Object.assign({}, state, {
+                status: action.type
             })
         }
 
